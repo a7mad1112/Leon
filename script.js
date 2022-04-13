531f9a6ab78ad3d079c0c4e098a628d5e0c86f51
Ahmed Shehab <ahmadshehab11177@outlook.sa>
2022-04-13 16:38:17 +0300

    vs code commit

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let linksUl = document.querySelector(".links-ul")
-let links = document.querySelector(".links")
-
-let c = 1;
-links.onclick = () => {
-    c+=1
-    if(c%2 ==0){
-
-        linksUl.style.display = "block"
-    }else{
-        linksUl.style.display = "none"
-
-    }
-
+function User (name,phone,email) {
+    this.name = name,
+    this.email = email,
+    this.phone = phone
 }
+
+const user1 = new User("ahmad" , `0592753581` , "ahmadshehab11177@gmail.com")
+const user2 = new User("ahmad" , `0592753581` , "ahmadshehab11177@gmail.com")
+const user3 = new User("ahmad" , `0592753581` , "ahmadshehab11177@gmail.com")
+const user4 = new User("ahmad" , `0592753581` , "ahmadshehab11177@gmail.com")
+const user5 = new User("ahmad" , `0592753581` , "ahmadshehab11177@gmail.com")
+console.log(user1.name)
+console.log(user1.email)
+console.log(user1.phone)
